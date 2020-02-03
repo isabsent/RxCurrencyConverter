8c852f5e52d3e1d1513d09cfcab73adbb42585e1
Ammar <s95ammar@gmail.com>
2020-02-03 19:13:04 +0200

    Added dependencies. Structured project.

--- app/src/androidTest/java/com/s95ammar/rxcurrencyconverter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/s95ammar/rxcurrencyconverter/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.s95ammar.rxcurrencyconverter;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -16,11 +17,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+	@Test
+	public void useAppContext() {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.s95ammar.rxcurrencyconverter", appContext.getPackageName());
-    }
+		assertEquals("com.s95ammar.rxcurrencyconverter", appContext.getPackageName());
+	}
 }
---
+++ app/src/main/java/com/s95ammar/rxcurrencyconverter/views/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.s95ammar.rxcurrencyconverter.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.s95ammar.rxcurrencyconverter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
